e519e22b7541d40fb2b64337bd4ae4495f0dda13
Rodrigo <rjcm1209.06@gmail.com>
2025-07-10 21:47:33 -0600

    password recovery terminado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ import Suscripciones from './pages/Suscripciones/Suscripciones'; // Asegúrate d
 import Products1 from './pages/Products/Products1'; // Asegúrate de que esta ruta sea correcta
 import Footer from './components/Footer/Footer'; // asegúrate de tener esta ruta correcta
 import Register from './pages/Register/Register'
+import RequestCode from './pages/RecoveryPassword/RequestCode'
+import VerifyCode from './pages/RecoveryPassword/VerifyCode';
+import NewPassword from './pages/RecoveryPassword/NewPasssword';
 
 function App() {
   return (
@@ -17,7 +20,12 @@ function App() {
           <Route path="/productos1" element={<Products1 />} />
           <Route path="/sobre-nosotros" element={<h1>Sobre Nosotros</h1>} />
           <Route path="/suscripciones" element={<Suscripciones />}/>
-          <Route path="/register" element={<Register />}/>
+          <Route path="/registro" element={<Register />}/>
+          <Route path="/enviar-codigo" element={<RequestCode/>}/>
+          <Route path="/verificar-codigo" element={<VerifyCode/>}/>
+          <Route path="/nueva-contraseña" element={<NewPassword/>}/>
+
+
         </Routes>
       </div>
       <Footer /> {/* Footer agregado */}
