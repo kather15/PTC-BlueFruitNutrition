cb5c3bf26712c07d542498de448b9b1e2a631ba5
Rodrigo <rjcm1209.06@gmail.com>
2025-07-10 18:50:03 -0600

    registro de clientes terminado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Products from './pages/AddProducts/Products';
 import Suscripciones from './pages/Suscripciones/Suscripciones'; // Asegúrate de que esta ruta sea correcta
 import Products1 from './pages/Products/Products1'; // Asegúrate de que esta ruta sea correcta
 import Footer from './components/Footer/Footer'; // asegúrate de tener esta ruta correcta
+import Register from './pages/Register/Register'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/productos1" element={<Products1 />} />
           <Route path="/sobre-nosotros" element={<h1>Sobre Nosotros</h1>} />
           <Route path="/suscripciones" element={<Suscripciones />}/>
+          <Route path="/register" element={<Register />}/>
         </Routes>
       </div>
       <Footer /> {/* Footer agregado */}
---
+++ frontend/src/pages/Register/Register.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import toast from "react-hot-toast";
+import VerifyCodeModal from "../../components/verifyCode/verifyCodeModal"
+import img from '../../assets/imgregister.png'
+import './Register.css'
+
+function Registro() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleRegresar = () => {
+    navigate("/");
+  };
+
+  const onSubmit = async (data) => {
+    try {
+      const res = await fetch("http://localhost:4000/api/registerCustomers", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include", // para guardar cookie con token
+        body: JSON.stringify(data),
+      });
+
+      const result = await res.json();
+
+      if (!res.ok) {
+        toast.error(result.message || "Error en el registro");
+        return;
+      }
+
+      toast.success("Registro exitoso. Revisa tu correo 📩");
+      setShowModal(true); // abrir modal de verificación
+    } catch (error) {
+      toast.error("Error de red o del servidor");
+    }
+  };
+
+  return (
+    <div className="registro-container">
+  <div className="imgre">
+    <img src={img} alt="Blue Fruit Nutrition" className="registro-img" />
+  </div>
+
+  <div className="registro-card">
+    <div className="registro-right">
+      <h1>Crear cuenta</h1>
+      <h2>Ingresa tus datos para continuar</h2>
+      <form className="registro-form" onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Nombre" {...register("name", { required: true })} />
+        <input type="text" placeholder="Apellido" {...register("lastname", { required: true })} />
+        <input type="email" placeholder="Correo Electrónico" {...register("email", { required: true })} />
+        <input type="date" placeholder="Fecha de nacimiento" {...register("dateBirth", { required: true })} />
+        <div className="registro-password-container">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Contraseña"
+            {...register("password", { required: true })}
+          />
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="toggle-password-btn"
+          >
+            {showPassword ? <FiEyeOff /> : <FiEye />}
+          </button>
+        </div>
+        <button type="submit" className="btn-crear">Crear Cuenta</button>
+      </form>
+      <p className="registro-login">
+        ¿Ya tienes una cuenta? <a href="/InicioSesion">Inicia Sesión</a>
+      </p>
+              <VerifyCodeModal isOpen={showModal} onClose={() => setShowModal(false)} />
+
+    </div>
+  </div>
+
+</div>
+
+   
+
+  );
+}
+
+export default Registro;
